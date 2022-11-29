1cf1e4c9398e9480a70fc7e68c54d4b833b16c81
JClark <justindanielclark@gmail.com>
2022-11-28 18:46:37 -0800

    Added Ability to Load Resources

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+import './style.css';
+
 const elvenShieldRecipe = {
   leatherStrips: 2,
   ironIngot: 1,
   refinedMoonstone: 4,
 }
-console.log(elvenShieldRecipe);
+
+for(let key in elvenShieldRecipe){
+  const p = document.createElement('p');
+  p.innerText = `${key}: ${elvenShieldRecipe[key]}`
+  document.body.append(p)
+}
